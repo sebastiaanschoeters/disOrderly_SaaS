352f925f332192c2bf8c4e5d24ce3bae25bc1eab
Sebastiaan Schoeters <sebastiaan.schoeters@student.kuleuven.be>
2024-10-18 16:26:57 +0200

    Supabase testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {createClient} from "@supabase/supabase-js"
+import {useEffect, useState} from "react";
+
+
+//const supabase = createClient("https://flsogkmerliczcysodjt.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsc29na21lcmxpY3pjeXNvZGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyNTEyODYsImV4cCI6MjA0NDgyNzI4Nn0.5e5mnpDQAObA_WjJR159mLHVtvfEhorXiui0q1AeK9Q")
 
 function App() {
+  /*
+    const [testData, setTestData] = useState([]);
+
+    useEffect(() => {
+      getData();
+    }, []);
+
+    async function getData() {
+      const {data} = await supabase.from("Test Table").select();
+      setTestData(data);
+    }
+
+   */
+
   return (
     <div className="App">
       <header className="App-header">

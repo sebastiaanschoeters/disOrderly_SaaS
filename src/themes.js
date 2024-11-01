c10679b7eb9e8a2ec8d877fad338ed39bf245a71
lottehermans <lotte.hermans@student.kuleuven.be>
2024-11-01 20:30:04 +0100

    Added new colors

--- src/themes.js
+++ src/themes.js
@@ -1,5 +1,5 @@
 // theme.js
-import { blue, green, red, purple, orange, magenta, cyan, greyDark } from '@ant-design/colors';
+import { blue, green, red, purple, orange, magenta, cyan, greyDark, lime, gold } from '@ant-design/colors';
 
 export const antThemeTokens = (colors) => ({
     colorPrimary: colors.primary7,         // Main color
@@ -32,6 +32,32 @@ export const themes = {
         primary8: cyan[7],
         primary9: cyan[8],
         primary10: cyan[9],
+    },
+
+    goud: {
+        primary1: gold[0],
+        primary2: gold[1],
+        primary3: gold[2],
+        primary4: gold[3],
+        primary5: gold[4],
+        primary6: gold[5],
+        primary7: gold[6],
+        primary8: gold[7],
+        primary9: gold[8],
+        primary10: gold[9],
+    },
+
+    limoen: {
+        primary1: lime[0],
+        primary2: lime[1],
+        primary3: lime[2],
+        primary4: lime[3],
+        primary5: lime[4],
+        primary6: lime[5],
+        primary7: lime[6],
+        primary8: lime[7],
+        primary9: lime[8],
+        primary10: lime[9]
     },
 
     donker: {

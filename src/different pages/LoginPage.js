2db60bc4ae136d97a9032238903035ad165683e2
wekkes123 <vanloo.wout@gmail.com>
2024-11-15 12:04:59 +0100

    todo plus fading

--- src/different pages/LoginPage.js
+++ src/different pages/LoginPage.js
@@ -4,81 +4,101 @@ import 'antd/dist/reset.css';
 import '../CSS/AntDesignOverride.css';
 import { antThemeTokens, themes } from '../themes';
 import { useNavigate } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import forestImage from '../Media/forest.jpg'
+import '../CSS/PageTransitions.css'; // Add CSS for transition effects
 
 const LoginPage = () => {
     const [theme, setTheme] = useState('default');
     const themeColors = themes[theme] || themes.blauw;
     const navigate = useNavigate();
+    const [isTransitioning, setIsTransitioning] = useState(false);
+
+    const handleLogin = () => {
+        setIsTransitioning(true); // Start the transition effect
+    };
+
+    const handleTransitionEnd = () => {
+        navigate('/home'); // Navigate after the transition completes
+    };
 
     return (
         <ConfigProvider theme={{ token: antThemeTokens(themeColors) }}>
-            <div
-                className="login-container"
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-                    backgroundColor: themeColors.primary2,
-                    color: themeColors.primary10,
-                    position: 'relative',
-                }}
+            <CSSTransition
+                in={!isTransitioning}
+                timeout={500}
+                classNames="fade"
+                onExited={handleTransitionEnd}
             >
-                {/* Activate Button */}
-                <Button
-                    type="link"
+                <div
+                    className="login-container"
                     style={{
-                        position: 'absolute',
-                        top: 20,
-                        right: 20,
-                        fontWeight: 'bold',
-                        color: themeColors.primary6,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100vh',
+                        backgroundImage:`url(${forestImage})`, // Initial background image
+                        backgroundColor: themeColors.primary3, // Transition to color
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        color: themeColors.primary10,
+                        position: 'relative',
+                        opacity: isTransitioning ? 0 : 1, // Fade out when transitioning
+                        transition: 'opacity 0.5s ease, background-color 0.5s ease',
+                        zIndex: '0',
                     }}
-                    onClick={() => navigate('/activate')}
                 >
-                    Activate
-                </Button>
-
-                <Card style={{ width: 300 }} title="Login" bordered={false}>
-                    <Form
-                        name="loginForm"
-                        initialValues={{ remember: true }}
-                        onFinish={(values) => console.log('Form values:', values)}
-                        onFinishFailed={(errorInfo) => console.log('Failed:', errorInfo)}
+                    {/* Activate Button */}
+                    <Button
+                        type="link"
+                        style={{
+                            position: 'absolute',
+                            top: 20,
+                            right: 20,
+                            fontWeight: 'bold',
+                            color: themeColors.primary6,
+                        }}
+                        onClick={() => navigate('/activate')}
                     >
-                        <Form.Item
-                            label="Email"
-                            name="email"
-                            rules={[{ required: true, message: 'Please input your email!' }]}
+                        Activate
+                    </Button>
+
+                    <Card style={{ width: 300 }} title="Login" bordered={false}>
+                        <Form
+                            name="loginForm"
+                            initialValues={{ remember: true }}
+                            onFinish={handleLogin}
+                            onFinishFailed={(errorInfo) => console.log('Failed:', errorInfo)}
                         >
-                            <Input />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your password!' }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
-
-                        <Form.Item name="remember" valuePropName="checked">
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-
-                        <Form.Item>
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                style={{ width: '100%' }}
-                                onClick={() => navigate('/home')}
+                            <Form.Item
+                                label="Email"
+                                name="email"
+                                rules={[{ required: true, message: 'Please input your email!' }]}
                             >
-                                Login
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </Card>
-            </div>
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item name="remember" valuePropName="checked">
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                                    Login
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Card>
+                </div>
+            </CSSTransition>
         </ConfigProvider>
     );
 };

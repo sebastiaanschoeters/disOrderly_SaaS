05a99822d7181bc088197c999a5bf4363dda0adf
Alvaro Geerken <alvaro.geerken@student.kuleuven.be>
2024-11-15 13:58:48 +0100

    HomePage.js added butterfly

--- src/different pages/HomePage.js
+++ src/different pages/HomePage.js
@@ -1,6 +1,6 @@
 import 'antd/dist/reset.css'; // Import Ant Design styles
 import '../CSS/AntDesignOverride.css'
-import { antThemeTokens, themes } from '../themes';
+import {antThemeTokens, ButterflyIcon, themes} from '../themes';
 import { Button, ConfigProvider } from 'antd';
 import { MessageOutlined, UserOutlined, SearchOutlined, SettingOutlined } from "@ant-design/icons";
 import React, { useState } from "react";
@@ -25,8 +25,10 @@ const HomePage = () => {
                     justifyContent: 'center',
                     alignItems: 'center',
                     flexDirection: 'column',
+                    zIndex: '0',
                 }}
             >
+                <ButterflyIcon color={themeColors.primary3} />
                 <div style={{
                     display: 'flex',
                     gap: '5%',

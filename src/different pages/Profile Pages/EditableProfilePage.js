ff7bb6e5893a0e3e598e01f66ad334a9ffb28e59
lottehermans <lotte.hermans@student.kuleuven.be>
2024-11-29 16:46:28 +0100

    Added home button to pages

--- src/different pages/Profile Pages/EditableProfilePage.js
+++ src/different pages/Profile Pages/EditableProfilePage.js
@@ -21,6 +21,7 @@ import {createClient} from "@supabase/supabase-js";
 import 'antd/dist/reset.css';
 import '../../CSS/AntDesignOverride.css';
 import '../../CSS/EditableProfilePage.css';
+import HomeButton from "../../Extra components/HomeButton";
 
 const supabase = createClient("https://flsogkmerliczcysodjt.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsc29na21lcmxpY3pjeXNvZGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyNTEyODYsImV4cCI6MjA0NDgyNzI4Nn0.5e5mnpDQAObA_WjJR159mLHVtvfEhorXiui0q1AeK9Q")
 
@@ -765,6 +766,7 @@ const ProfileCard = () => {
                 color: themeColors.primary10,
                 zIndex: '0'
             }}>
+                <HomeButton color={themeColors.primary7} />
                 <ButterflyIcon color={themeColors.primary3}/>
 
                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', paddingBottom: '20px'}}>

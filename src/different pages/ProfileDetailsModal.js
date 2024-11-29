48b283656e71f7e7abd5db7ea69c49634b831b62
lottehermans <lotte.hermans@student.kuleuven.be>
2024-11-29 12:48:11 +0100

    Fixed search profile popup

---
+++ src/different pages/ProfileDetailsModal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Modal } from "antd";
+import ProfileCard from './ProfilePage'
+
+const ProfileDetailsModal = ({ visible, onClose, clientData }) => {
+    if (!clientData) return null; // Guard for no data
+    console.log(clientData)
+    return (
+        <Modal
+            title="Client Details"
+            open={visible}
+            onCancel={onClose}
+            footer={null}
+            width='90%'
+            style={{
+                position: 'relative',
+                overflow: 'hidden'
+            }}
+        >
+            <div>
+                <ProfileCard user_id={clientData.id} viewedByCareteaker={false}/>
+            </div>
+        </Modal>
+    );
+};
+
+export default ProfileDetailsModal;

5caf86c66245663cf550b91ca3b1ee98461926cb
lottehermans <lotte.hermans@student.kuleuven.be>
2024-11-29 11:54:34 +0100

    modal public profile done

--- src/different pages/ClientDetailsModal.js
+++ src/different pages/ClientDetailsModal.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { Modal } from "antd";
+import ProfileCard from './ProfilePage'
 
 const ClientDetailsModal = ({ visible, onClose, clientData }) => {
     if (!clientData) return null; // Guard for no data
+
+    const renderContentByAccessLevel = (accessLevel) => {
+        switch (accessLevel) {
+            case "Volledige toegang":
+                return <p>You have full access to all features and data.</p>;
+            case "Gesprekken":
+                return <p>You have access to conversations.</p>;
+            case "Contacten":
+                return <p>You have access to contact information.</p>;
+            case "Publiek Profiel":
+                return <ProfileCard user_id={clientData.id} viewedByCareteaker={true} />;
+            default:
+                return <p>Access level information is not available.</p>;
+        }
+    };
 
     return (
         <Modal
             title="Client Details"
             open={visible}
             onCancel={onClose}
-            onOk={onClose}
-            footer={null} // Customize footer as needed
+            footer={null}
+            width='90%'
+            style={{
+                position: 'relative',
+                overflow: 'hidden'
+            }}
         >
             <div>
-                <p><strong>Name:</strong> {clientData.client_info.name}</p>
-                <p><strong>Access Level:</strong> {clientData.access_level}</p>
-                {/* Add more client details here as needed */}
+                {renderContentByAccessLevel(clientData.access_level)}
             </div>
         </Modal>
     );
